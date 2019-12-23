4bb639d7176ea24f952dd9cee7bc91dd546c6e87
Joshua Ogle <joshua@joshuaogle.com>
2019-12-23 15:00:18 -0600

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -2,11 +2,12 @@ use Mix.Config
 
 config :samwise, Samwise.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "samwise-api.husk.co", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "samwise-api.husk.co", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
-config :peepchat, Peepchat.Repo,
+config :samwise, Samwise.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20
--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Samwise.Mixfile do
 
   def project do
     [app: :samwise,
-     version: "0.0.1",
-     elixir: "~> 1.2",
+     version: "0.2.0",
+     elixir: "~> 1.6.6",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
